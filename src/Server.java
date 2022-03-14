bf43833f3a85d312b871dedef8baeb9190e55ddb
Demarcily <60388907+Demarcily@users.noreply.github.com>
2022-03-14 09:42:18 +0100

    Start

---
+++ src/Server.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+  public static void main(String[] args){
+    int port = 8000;
+    boolean run = true;
+    ServerSocket serverSocket;
+    Socket socket;
+    System.out.println("Server started.");
+
+    try {
+      serverSocket = new ServerSocket(port);
+      while (true) {
+        System.out.println("Waiting for connections!");
+        socket = serverSocket.accept();
+        // Go
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        //ListenerThread in =
+        //        new ListenerThread(new BufferedReader(new InputStreamReader(socket.getInputStream())));
+        //Thread listener = new Thread(in);
+        //listener.start();
+        System.out.println("Client connected!");
+        Scanner tgb = new Scanner(System.in);
+        //Protocol
+        while (run) {
+          out.println("SERVER: Welcome! \n What's your name?");
+          String msg = in.readLine();
+          if (msg.equals("quit")) {
+            run = false;
+          } else {
+            System.out.println(msg);
+          }
+        }
+        out.close();
+        socket.close();
+      }
+    } catch (IOException e) {
+      System.out.println("Server fail");
+    }
+  }
+}
